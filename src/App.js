aaa858760ee1967b6df41943d9857faffc702127
Asendia Mayco Himawan <asendia@traveloka.com>
2018-09-07 19:34:26 +0700

    Add netlify id widget

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import netlifyIdentity from 'netlify-identity-widget';
 import './App.css';
 
 class App extends Component {
+  handleRegister = () => {
+    netlifyIdentity.open();
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +15,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to cloudtestament</h1>
         </header>
+        <p className="App-intro">
+          <a href="#" onClick={}>Register.</a>
+        </p>
         <p className="App-intro">
           It is still WIP, visit <a href="https://www.fjbgame.com/contact">fjbgame</a> if you want to contact me.
         </p>
