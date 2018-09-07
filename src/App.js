9b833acdce6ed1b28a995cf14e6bc4a8e73b06b4
Asendia Mayco Himawan <asendia@traveloka.com>
2018-09-07 20:03:54 +0700

    Fix netlify id modal doesn't show

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import logo from './logo.svg';
 import netlifyIdentity from 'netlify-identity-widget';
 import './App.css';
 
+netlifyIdentity.init({
+  container: "#netlify-modal" // defaults to document.body,
+});
+netlifyIdentity.on("init", user => console.log(user));
+netlifyIdentity.on("login", user => console.log(user));
+netlifyIdentity.on("logout", () => console.log("Logged out"));
+netlifyIdentity.on("error", err => console.error("Logged out"));
+netlifyIdentity.on("open", () => console.log("Widget opened"));
+netlifyIdentity.on("close", () => console.log("Widget closed"));
+
 class App extends Component {
   handleRegister = () => {
-    netlifyIdentity.open();
+    netlifyIdentity.open('login');
   }
 
   render() {
@@ -16,7 +26,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to cloudtestament</h1>
         </header>
         <p className="App-intro">
-          <button onClick={this.handleRegister}>Register.</button>
+          <button onClick={this.handleRegister}>Login</button>
         </p>
         <p className="App-intro">
           It is still WIP, visit <a href="https://www.fjbgame.com/contact">fjbgame</a> if you want to contact me.
